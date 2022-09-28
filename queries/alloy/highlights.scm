1034c8bec691cc0146e6210d7227e7608ab03ecf
Ethan Rooke <ethan@roo.ke>
2022-09-28 02:45:08 -0500

    Add tree-sitter queries

---
+++ queries/alloy/highlights.scm
@@ -0,0 +1,61 @@
+[
+  "'"
+  "+"
+  ";"
+  "and"
+  "in"
+  "not"
+  "or"
+  "#"
+] @operator
+
+[
+  "abstract"
+  "after"
+  "always"
+  "before"
+  "but"
+  "check"
+  "disj"
+  "enum"
+  "eventually"
+  "exactly"
+  "extends"
+  "fact"
+  "for"
+  "fun"
+  "historically"
+  "iden"
+  "iff"
+  "let"
+  "lone"
+  "module"
+  "no"
+  "none"
+  "once"
+  "one"
+  "pred"
+  "releases"
+  "run"
+  "set"
+  "sig"
+  "since"
+  "some"
+  "sum"
+  "triggered"
+  "univ"
+  "until"
+  "var"
+] @keyword
+
+[
+ "iden"
+ "none"
+ "univ"
+] @constant.builtin
+
+(number) @number
+(comment) @comment
+
+(decl
+  type: (_) @type)
